5e5028823d5ee2030f47137f36fc9ce7f1782dac
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-01-09 13:29:22 -0600

    routing update

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ import express from 'express'; //modern notation, put' "type": "module",' in pac
 import cors from 'cors'; // Rules between frontend and backend.
 import mongoose from 'mongoose'; // Database management system, allows queries to database. 
 
+import {userRouter} from './routes/users.js'
+
 const app = express(); //generate version of API.
 
 app.use(express.json()); //JSON middleware. data sent from frontend is converted into JSON with every request.
 app.use(cors()); //Resolves issues with API requests with frontend.
+
+app.use("/auth", userRouter); // Seperating code to write endpoints related to authentication will exist in users.js
 
 mongoose.connect("mongodb+srv://brycetown10:T44MAyOhrGnIepnE@notesapp.qyr1dbx.mongodb.net/") // Connects to server. 
 .then(() => console.log('Connected to MongoDB'))
