36b4bc5ea7aed69fb8265a43986e074a27811a48
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-01-09 21:46:23 -0600

    works.

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,8 +1,10 @@
+// models/Users.js
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({ //Schema an object that defines structure of data.
     username:{type: String, required: true, unique:true}, //username field that must be a string, it is required to be a string and must be unique. 
     password: {type: String, required: true},
+    savedNotes: [{type: mongoose.Schema.Types.ObjectId, ref: "Note"}],
 });
 
 export const UserModel = mongoose.model("users", UserSchema) //Model generated based off schema. Setting Schema to be a collection. "users" is called from the database. 
