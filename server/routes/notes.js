e391a1e9fb953b9e1c2c108efbd87ae9c1c2d84c
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-01-09 18:59:47 -0600

    get request notes

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -25,5 +25,16 @@ router.post("/create-note", verifyToken, async (req, res) => {
   }
 });
 
+// Fetch all notes
+router.get("/get-notes", verifyToken, async (req, res) => {
+  try {
+    const notes = await NoteModel.find();
+    res.status(200).json({ notes });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error", error: error.message });
+  }
+});
+
 export { router as noteRouter };
 
