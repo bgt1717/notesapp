ca5fc1dff8d9f9f9bacdb7c422f98e5f85c7b615
bryce <bgt17@txstate.edu>
2024-01-13 16:32:24 -0600

    navbar updated

--- web/src/components/navbar.jsx
+++ web/src/components/navbar.jsx
@@ -1,32 +1,36 @@
-import { Link } from "react-router-dom"
-import {useCookies} from "react-cookie";
+import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-    const [cookies, setCookies] = useCookies(["access_token"]);
-    const navigate = useNavigate(); //useNavigate Hook.
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate(); // useNavigate Hook.
 
-    const logout = () => {
-        setCookies("access_token", "") //Set cookies to an empty token.
-        window.localStorage.removeItem("userID"); //Clears local Storage Key.
-        navigate("/auth"); //Calls Hook to route to /auth page.
-    }
+  const logout = () => {
+    setCookies("access_token", ""); // Set cookies to an empty token.
+    window.localStorage.removeItem("userID"); // Clears local Storage Key.
+    navigate("/auth"); // Calls Hook to route to /auth page.
+  };
+
   return (
     <div className="navbar">
-      <Link to="/"> Home </Link>
-      
+      <div className="logo">
+        <Link to="/">MyNotes</Link>
+      </div>
 
-      {!cookies.access_token ? (<Link to="/auth"> Login/Register </Link> //If there is not an access token in cookies, route to Login/Register page.  
-      ) : ( 
-        <>
-      <Link to="/createnote"> Create Note </Link>
-      <Link to="/savedNotes"> Saved Notes </Link>
-
-      <button onClick={logout}>Logout </button>                   
-      </> //Otherwise, Display a button to Logout. When pressed, it calls the logout function.
-      )}
+      <div className="nav-links">
+        {!cookies.access_token ? (
+          <Link to="/auth">Login/Register</Link>
+        ) : (
+          <>
+            <Link to="/createnote">Create Note</Link>
+            <Link to="/savedNotes">Saved Notes</Link>
+            <button onClick={logout}>Logout</button>
+          </>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
