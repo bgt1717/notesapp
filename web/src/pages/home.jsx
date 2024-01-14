b51d5085c563c2f37aba9ab247795442a98cbf12
bryce <bgt17@txstate.edu>
2024-01-14 17:04:16 -0600

    homepage update.   Navigation

--- web/src/pages/home.jsx
+++ web/src/pages/home.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [cookies] = useCookies(['access_token']);
+
+  const handleButtonClick = () => {
+    navigate('/auth');
+  };
+
   return (
     <div className="HomeContainer">
       <div className="CenteredColumn">
-        <p className="CenteredText">
+        <div className="CenteredText">
           Capture ideas, organize thoughts, and never miss a moment with our intuitive notes app.
-        </p>
-        <p>
-          My Notes is an easy-to-use application for notetaking. Made with node.JS, MongoDB, EJS, React, and MongoDB.
-        </p>
+        </div>
+        <div className="CenteredTextSmall">
+          My Notes is an easy-to-use application for notetaking. 
+          Made with node.JS, MongoDB, EJS, React, and MongoDB.
+        </div>
       </div>
-      <button>Try MyNotes, It's FREE!</button>
+      {!cookies.access_token && (
+        <button className="YellowButton" onClick={handleButtonClick}>
+          Try MyNotes, It's FREE!
+        </button>
+      )}
     </div>
   );
 }
 
 export default Home;
+
 
