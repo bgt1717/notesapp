da5f2e53ed72936b318f4fff47bd928bb4c8044d
bryce <bgt17@txstate.edu>
2024-01-14 12:45:50 -0600

    landing page update

--- web/src/pages/home.jsx
+++ web/src/pages/home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const home = () => {
+const Home = () => {
   return (
     <div className="HomeContainer">
-      hi
+      <div className="CenteredColumn">
+        <p className="CenteredText">
+          Capture ideas, organize thoughts, and never miss a moment with our intuitive notes app.
+        </p>
+        <p>
+          My Notes is an easy-to-use application for notetaking. Made with node.JS, MongoDB, EJS, React, and MongoDB.
+        </p>
+      </div>
+      <button>Try MyNotes, It's FREE!</button>
     </div>
-  )
+  );
 }
 
-export default home
+export default Home;
+
